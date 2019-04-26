6deded4cbf7ae33e0d9df2442079727e91297ea7
Sneha KS <snehaa.ks@gmail.com>
2019-04-26 19:20:09 -0400

    Finished Usecase 1

--- main.py
+++ main.py
@@ -17,11 +17,15 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
-@app.route('/blog', methods=['POST', 'GET'])
+@app.route('/blog')
 def index():
 
-    blogs = Blog.query.all()
-    return render_template('main_page.html',title='Build a Blog', blogs=blogs)
+    blog_id = request.args.get('id')
+    if blog_id:
+        return redirect('/blogpage')
+    else:
+        blogs = Blog.query.all()
+        return render_template('main_page.html',title='Build a Blog', blogs=blogs)
 
 
 @app.route('/newpost', methods=['POST','GET'])
@@ -30,13 +34,35 @@ def newpost():
     if request.method == 'POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
-        new_blog = Blog(blog_title, blog_body)
-        db.session.add(new_blog)
-        db.session.commit()
-        return redirect('/blog')
+
+        titleError = ''
+        bodyError = ''
+
+        #Title Validation
+        if not blog_title:
+            titleError = 'Please fill in the title'
+
+        #Body Validation
+        if not blog_body:
+            bodyError = 'Please fill in the body'
+            
+        if not titleError and not bodyError:
+            new_blog = Blog(blog_title, blog_body)
+            db.session.add(new_blog)
+            db.session.commit()
+            return redirect('/blog')
+        else:
+            return render_template('new_post.html', title='New Blog', blog_title=blog_title,
+            titleError=titleError, blog_body=blog_body, bodyError=bodyError)
     else:
         return render_template('new_post.html',title='New Blog')
 
+@app.route('/blogpage')
+def display():
+    
+    blog_id = int(request.args.get('id'))
+    blog= Blog.query.filter_by(id=blog_id).first()
+    return render_template('blog_page.html', blog=blog)
 
 if __name__ == '__main__':
     app.run()
